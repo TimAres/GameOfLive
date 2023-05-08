d891e9483aaa438b92452424c01282d7d69b362b
tbhaa <tbhaa@Tims-Desktop.speedport.ip>
2023-05-08 20:24:37 +0200

    Alle Methoden in LifeThreadPool aktualisiert und vervollstaendigt. Leider Compiliert er noch nicht so ganz.

--- gol/src/main/java/de/hawhamburg/inf/gol/LifeThreadPool.java
+++ gol/src/main/java/de/hawhamburg/inf/gol/LifeThreadPool.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 import java.util.stream.Stream;
 
 /**
- * A pool of LifeThreads and a queue holding Runnables to be processed.
+ * A pool of LifeThreads and a queue holding  to be processed.
  * 
  * @author Christian Lins
  */
@@ -55,9 +55,11 @@ public class LifeThreadPool {
     /**
      * Calls interrupt() on every thread in this pool.
      */
-    public void interrupt() {
-        // TODO Nutzen Sie Streams!
+    public void interrupt()
+    {
+    Stream.of(threads).forEach(Thread::interrupt);
     }
+
     
     /**
      * Waits for all tasks to finish and calls interrupt on every thread. This
@@ -65,16 +67,21 @@ public class LifeThreadPool {
      * 
      * @throws InterruptedException 
      */
-    public void joinAndExit() throws InterruptedException {
-        // TODO
-    
+    public void joinAndExit() throws InterruptedException
+    {
+        barrier(); 
+        interrupt(); 
+    }
     /**
      * Adds a task to the queue of this pool.
      * 
      * @param task Runnable containing the work to be done 
      */
-    public void submit(Runnable task) {
-        // TODO
+    public void submit(Runnable task)
+    {
+        // Sicherheit gegen Ueberlastung der Queue 
+        tasks.offer(task); 
+        tasks.notifyAll(); 
     }
     
     /**
@@ -85,16 +92,21 @@ public class LifeThreadPool {
      * @throws InterruptedException 
      */
     public Runnable nextTask() throws InterruptedException {
-        // TODO
+        synchronized (tasks)
+        {
+            while(tasks.isEmpty())
+            {
+                tasks.wait();
+            }
+            return tasks.poll(); 
+        }
     }
     
-    /**
-     * Start all threads in this pool.
-     */
+    
     public void start() {
         for (int i = 0; i < numThreads; i++) {
-            threads[i] = ..
-            // TODO
+            threads[i] = new LifeThread(this); 
+            threads[i].start(); 
         }
     }
 }
